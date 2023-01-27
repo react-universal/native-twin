50d5bafdfe9096db7e5b99edf3e058db2970a578
chrisarts <chrisarts@users.noreply.github.com>
2023-01-27 13:39:53 -0500

    feat(primitives): pressable

---
+++ packages/primitives/nativewind.pressable/README.md
@@ -0,0 +1,7 @@
+# primitives-nativewind.pressable
+
+This library was generated with [Nx](https://nx.dev).
+
+
+
+
---
+++ packages/primitives/nativewind.pressable/src/index.ts
@@ -0,0 +1,3 @@
+export function primitivesNativewindPressable(): string {
+    return 'primitives-nativewind.pressable';
+}
