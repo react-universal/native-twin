90886a2654845de484e33f2fab66e8576a8525d8
chrisarts <chrisarts@users.noreply.github.com>
2023-01-27 13:58:47 -0500

    feat(generators): expo-lib nx generator

---
+++ packages/nx/expo-library/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx-expo-library',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    }
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/packages/nx/expo-library'
+};
---
+++ packages/nx/expo-library/src/generators/nx-expo-library/schema.d.ts
@@ -0,0 +1,5 @@
+export interface NxExpoLibraryGeneratorSchema {
+    name: string;
+    tags?: string;
+    directory?: string;
+}
