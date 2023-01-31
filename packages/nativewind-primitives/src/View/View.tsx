ba7f852e858a56204795df2a08d50c6c24ce7402
chrisarts <chrisarts@users.noreply.github.com>
2023-01-30 19:11:43 -0500

    packages

--- packages/nativewind-primitives/src/View/View.tsx
+++ packages/nativewind-primitives/src/View/View.tsx
@@ -2,7 +2,7 @@ import { forwardRef } from 'react';
 import { View as ReactNativeView, ViewProps as NativeViewProps } from 'react-native';
 import { TW, createStyledComponent, mergeTWClasses } from '@react-universal/nativewind-utils';
 
-type ViewProps = Omit<NativeViewProps, 'tw' | 'className'> & {
+type ViewProps = Omit<NativeViewProps, 'tw'> & {
   tw?: string | Array<string> | TW[];
 };
 
