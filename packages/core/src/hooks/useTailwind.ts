c8dcf3d584b8e8b3e16220de650f800a597341ba
chrisarts <chrisarts@users.noreply.github.com>
2023-02-14 00:37:10 -0500

    adding component interactions internal with HMR

--- packages/core/src/hooks/useTailwind.ts
+++ packages/core/src/hooks/useTailwind.ts
@@ -1,17 +1,14 @@
-import { useId } from 'react';
 import type { IRegisterComponentArgs } from '../types/store.types';
 import { useComponentRegistration } from './useComponentRegistration';
 
 function useTailwind(data: Omit<IRegisterComponentArgs, 'id'>) {
-  const id = useId();
   const { component, hasInteractions, interactionStyles } = useComponentRegistration({
-    id,
     inlineStyles: data.inlineStyles,
     className: data.className,
   });
 
   return {
-    id,
+    id: component.id,
     styles: component.styles,
     hasInteractions,
     interactionStyles,
