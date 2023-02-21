3b1c8970c6e00b005706edc76550f1650784b392
chrisarts <chrisarts@users.noreply.github.com>
2023-02-21 01:28:50 -0500

    adding tailwind compiler to RN

---
+++ packages/tailwind/test/tailwindParser.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from 'vitest';
+import { setup } from '../src';
+
+const tw = setup({ content: ['__'] });
+
+describe('TailwindCSS compiler', () => {
+  it('Normal color', () => {
+    const css = tw('bg-black');
+    expect(css.JSS).toStrictEqual({
+      '.bg-black': {
+        backgroundColor: 'rgba(0,0,0,1)',
+      },
+    });
+  });
+
+  it('Color with opacity', () => {
+    const css = tw('bg-black/50');
+    expect(css.JSS).toStrictEqual({
+      '.bg-black\\/50': {
+        backgroundColor: 'rgba(0,0,0,0.5)',
+      },
+    });
+  });
+});
