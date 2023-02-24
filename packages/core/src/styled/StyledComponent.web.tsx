24c74eab8db850a75b9cec68da2b3c2e06d2b2df
chrisarts <chrisarts@users.noreply.github.com>
2023-02-23 22:37:55 -0500

    adding state as animated values

--- packages/core/src/styled/StyledComponent.web.tsx
+++ packages/core/src/styled/StyledComponent.web.tsx
@@ -5,52 +5,57 @@ import { mergeTWClasses } from '../utils/mergeClasses';
 const styled = (Component: ComponentType) => {
   const classProps: string[] = [];
   const extraTransform: Record<string, string> = {};
+  const createAnimatedComponent = () => {
+    const Styled = forwardRef<unknown, any>(function StyledTW(
+      { tw, className, ...props },
+      ref,
+    ) {
+      let processedProps = props;
+      const transformClassValue: string[] = useMemo(() => [], []);
 
-  const Styled = forwardRef<unknown, any>(function ({ tw, className, ...props }, ref) {
-    let processedProps = props;
-    const transformClassValue: string[] = useMemo(() => [], []);
+      if (classProps.length > 0) {
+        processedProps = {};
 
-    if (classProps.length > 0) {
-      processedProps = {};
+        for (const [key, value] of Object.entries(props)) {
+          if (extraTransform[key] && typeof value === 'string') {
+            const newKey = key === extraTransform[key] ? key : extraTransform[key];
 
-      for (const [key, value] of Object.entries(props)) {
-        if (extraTransform[key] && typeof value === 'string') {
-          const newKey = key === extraTransform[key] ? key : extraTransform[key];
-
-          processedProps[newKey] = { $$css: true, [value]: value };
-        } else if (classProps.includes(key) && typeof value === 'string') {
-          transformClassValue.push(value);
-        } else {
-          processedProps[key] = value;
+            processedProps[newKey] = { $$css: true, [value]: value };
+          } else if (classProps.includes(key) && typeof value === 'string') {
+            transformClassValue.push(value);
+          } else {
+            processedProps[key] = value;
+          }
         }
       }
+
+      const style: IStyleType = useMemo(() => {
+        const mergedClassName = mergeTWClasses(
+          [transformClassValue.join(' '), className ?? tw].join(' '),
+        );
+
+        if (mergedClassName && props.style) {
+          return [
+            { $$css: true, [mergedClassName]: mergedClassName },
+            props.style,
+          ] as IStyleType;
+        } else if (mergedClassName) {
+          return { $$css: true, [mergedClassName]: mergedClassName } as IStyleType;
+        } else if (style) {
+          return style as IStyleType;
+        }
+        return {};
+      }, [props.style, className, tw, transformClassValue]);
+
+      return <Component ref={ref} style={style} {...processedProps} />;
+    });
+
+    if (typeof Component !== 'string') {
+      Styled.displayName = `Tailwind.${Component.displayName || Component.name || 'NoName'}`;
     }
-
-    const style: IStyleType = useMemo(() => {
-      const mergedClassName = mergeTWClasses(
-        [transformClassValue.join(' '), className ?? tw].join(' '),
-      );
-
-      if (mergedClassName && props.style) {
-        return [
-          { $$css: true, [mergedClassName]: mergedClassName },
-          props.style,
-        ] as IStyleType;
-      } else if (mergedClassName) {
-        return { $$css: true, [mergedClassName]: mergedClassName } as IStyleType;
-      } else if (style) {
-        return style as IStyleType;
-      }
-      return {};
-    }, [props.style, className, tw, transformClassValue]);
-
-    return <Component ref={ref} style={style} {...processedProps} />;
-  });
-
-  if (typeof Component !== 'string') {
-    Styled.displayName = `Tailwind.${Component.displayName || Component.name || 'NoName'}`;
-  }
-  return Styled;
+    return Styled;
+  };
+  return createAnimatedComponent;
 };
 
 export { styled };
