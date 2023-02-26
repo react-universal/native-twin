ca52fcc4b74f2ab7bb0434951e06986707e790a9
chrisarts <chrisarts@users.noreply.github.com>
2023-02-26 03:07:18 -0500

    aspect ratio native

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,7 +4,6 @@ export type {
   IRegisterComponentArgs,
 } from './types/store.types';
 export { useIsDarkMode, useStyledComponent } from './hooks';
-export { stylesStore as tailwindStore } from './store';
 export type { IStyleType, IStyleTuple } from './types/styles.types';
 export type { TPseudoSelectorTypes } from './types/store.types';
 export { styled } from './styled';
