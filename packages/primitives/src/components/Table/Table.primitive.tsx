6fea5f2a9602895a2c7993dd23c25d9409fb16e0
Cristhian Gutierrez <chrisarts.net@gmail.com>
2023-03-14 11:27:37 -0500

    html elements created

---
+++ packages/primitives/src/components/Table/Table.primitive.tsx
@@ -0,0 +1,13 @@
+import type { ClassAttributes, ComponentProps, ComponentType } from 'react';
+import Text from '../Text/Text.primitive';
+
+type NativeTextProps = ComponentProps<typeof Text> & ClassAttributes<typeof Text>;
+
+export type TableTextProps = NativeTextProps & {
+  /** @platform web */
+  colSpan?: number | string;
+  /** @platform web */
+  rowSpan?: number | string;
+};
+
+export const TableText = Text as ComponentType<TableTextProps>;
