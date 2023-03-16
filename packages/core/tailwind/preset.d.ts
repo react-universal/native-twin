4f2b94d214d20a3916649349c030e353a813eddf
Cristhian Gutierrez <chrisarts.net@gmail.com>
2023-03-15 22:54:29 -0500

    core library now exports a tailwind preset for react native

---
+++ packages/core/tailwind/preset.d.ts
@@ -0,0 +1,7 @@
+import type { Config } from 'tailwindcss';
+
+export interface IPresetArgs {
+  baseRem?: number;
+}
+export function reactNativeTailwindPreset(input?: IPresetArgs): Config;
+//# sourceMappingURL=tailwind-preset.d.ts.map
