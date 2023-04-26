869533567e1bb15b4934974f3f9639bb30ac6701
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2023-04-25 20:55:37 -0400

    Format tests files

--- packages/twind-adapter/test/layout.test.ts
+++ packages/twind-adapter/test/layout.test.ts
@@ -10,7 +10,7 @@ describe('TailwindCSS layout', () => {
     const { css } = transformClassNames('w-4');
     expect(css).toStrictEqual('/*!dbgidc,v,w-4*/.w-4{width}')
   });
-  
+
   it('Height', () => {
     const { css } = transformClassNames('h-4');
     expect(css).toStrictEqual('/*!dbgidc,v,h-4*/.h-4{height}')
