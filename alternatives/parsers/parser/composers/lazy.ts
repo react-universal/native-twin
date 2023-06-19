6b049864d62cc99c098ff68f673301309babb8cc
Cristhian Gutierrez <chrisarts.net@gmail.com>
2023-06-19 16:38:36 -0500

    feat(css): css parser lib

--- alternatives/parsers/parser/composers/lazy.ts
+++
@@ -1,8 +0,0 @@
-import { Parser } from '../Parser';
-
-export const lazyParserMatch = (parserThunk: () => Parser) => {
-  return new Parser((parserState) => {
-    const parser = parserThunk();
-    return parser.parserStateTransformerFn(parserState);
-  });
-};
--- alternatives/parsers/parser/types.ts
+++
@@ -1,9 +0,0 @@
-export type ParserFn = (parserState: ParserState) => ParserState;
-
-export interface ParserState {
-  index: number;
-  targetString: string;
-  result: string | any;
-  isError: boolean;
-  error: string | null;
-}
