0f6392a1480c064f8e26049de929c7da5a790255
Cristhian Gutierrez <chrisarts.net@gmail.com>
2023-06-21 13:03:58 -0500

    feat(css): css parser lib

--- packages/css/test/CssLexer.test.ts
+++ packages/css/test/CssLexer.test.ts
@@ -11,7 +11,7 @@ describe('@universal-labs/stylesheets', () => {
     tw.clear();
   });
   it('CSS Lexer', () => {
-    tx('flex-1 leading-6 text-2xl -mt-[10vw] translate-y-2 hover:text-lg sm:text-lg');
+    tx('flex-1 leading-6 text-2xl -mt-[10vw] translate-y-2 hover:text-lg');
     const result = tw.target.map((x) =>
       parseCssString(x, {
         deviceHeight: 1280,
