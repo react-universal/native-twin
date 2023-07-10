c27741ca1aab7f7fdb2dd224ad1f2e28aad1b72a
Cristhian Gutierrez <chrisarts.net@gmail.com>
2023-07-10 12:14:41 -0500

    feat(tests): tests fix

--- packages/css/test/CssLexer.test.ts
+++ packages/css/test/CssLexer.test.ts
@@ -1,29 +1,11 @@
-import { initialize } from '@universal-labs/twind-adapter';
-import { afterEach, describe, expect, it } from 'vitest';
-import { CssResolver } from '../src';
-import { sheetManager } from '../src/data/SheetManager';
-import { createParserContext } from '../src/parsers/Parser';
+import { describe, expect, it } from 'vitest';
 // eslint-disable-next-line unused-imports/no-unused-imports
-import { inspectTestElement } from './test-utils';
-
-const { tx, tw } = initialize();
-const { context } = createParserContext({
-  deviceHeight: 1280,
-  deviceWidth: 720,
-  rem: 16,
-  platform: 'ios',
-});
-
-const resolver = sheetManager(context);
+import { generateStylesFor, inspectTestElement } from './test-utils';
 
 describe('@universal-labs/css - AT-RULES', () => {
-  afterEach(() => {
-    tw.clear();
-  });
   it('at-rule', () => {
-    tx('text-2xl sm:text-base');
-    const result = CssResolver(tw.target, context);
-    // inspectTestElement('text-2xl sm:text-base', tw.target, result);
+    const result = generateStylesFor('text-2xl sm:text-base');
+    // inspectTestElement('text-2xl sm:text-base', [], result);
 
     expect(result.base).toStrictEqual({
       fontSize: 16,
@@ -33,23 +15,14 @@ describe('@universal-labs/css - AT-RULES', () => {
 });
 
 describe('@universal-labs/css Resular Rules', () => {
-  afterEach(() => {
-    tw.clear();
+  it('text-2xl', () => {
+    const result = generateStylesFor('text-2xl');
+    // inspectTestElement('text-2xl rotate-[1.2turn]', [], result);
+    expect(result.base).toStrictEqual({ fontSize: 24, lineHeight: 32 });
   });
 
   it('text-2xl', () => {
-    tx('text-2xl');
-    const result = resolver.parse(tw.target[0]!);
-    // inspectTestElement('text-2xl rotate-[1.2turn]', tw.target, result);
-    expect(result).toStrictEqual({
-      selector: { group: 'base', value: '.text-2xl' },
-      declarations: { fontSize: 24, lineHeight: 32 },
-    });
-  });
-
-  it('text-2xl', () => {
-    tx('text-2xl rotate-[1.2turn]');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('text-2xl rotate-[1.2turn]');
     // inspectTestElement('text-2xl rotate-[1.2turn]', tw.target, result.base);
     expect(result.base).toStrictEqual({
       fontSize: 24,
@@ -59,22 +32,19 @@ describe('@universal-labs/css Resular Rules', () => {
   });
 
   it('skew-x-1', () => {
-    tx('skew-x-1');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('skew-x-1');
     // inspectTestElement('skew-x-1', tw.target, result.base);
     expect(result.base).toStrictEqual({ transform: [{ skewX: '1deg' }] });
   });
 
   it('font-sans', () => {
-    tx('font-sans');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('font-sans');
     // inspectTestElement('text-2xl rotate-[1.2turn]', tw.target, result.base);
     expect(result.base).toStrictEqual({ fontFamily: 'ui-sans-serif' });
   });
 
   it('shadow', () => {
-    tx('shadow-md');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('shadow-md');
     // inspectTestElement('shadow', tw.target, result);
 
     expect(result.base).toStrictEqual({
@@ -86,8 +56,7 @@ describe('@universal-labs/css Resular Rules', () => {
   });
 
   it('flex-1 aspect-square', () => {
-    tx('flex-1 aspect-square');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('flex-1 aspect-square');
     // inspectTestElement('flex-1', tw.target, result);
 
     expect(result.base).toStrictEqual({
@@ -99,70 +68,61 @@ describe('@universal-labs/css Resular Rules', () => {
   });
 
   it('bg-gray-200', () => {
-    tx('bg-gray-200');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('bg-gray-200');
     // inspectTestElement('bg-gray-200', tw.target, result);
     expect(result.base).toStrictEqual({ backgroundColor: 'rgba(229,231,235,1)' });
   });
 
   it('-mt-2', () => {
-    tx('-mt-2');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('-mt-2');
     // inspectTestElement('-mt-2', tw.target, result);
 
     expect(result.base).toStrictEqual({ marginTop: -8 });
   });
 
   it('gap', () => {
-    tx('gap-2 gap-x-2 gap-y-2');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('gap-2 gap-x-2 gap-y-2');
     // inspectTestElement('gap-2', tw.target, result);
 
     expect(result.base).toStrictEqual({ gap: 8, columnGap: 8, rowGap: 8 });
   });
 
   it('translate-x-2', () => {
-    tx('translate-x-2');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('translate-x-2');
     // inspectTestElement('translate-x-2', tw.target, result);
 
     expect(result.base).toStrictEqual({ transform: [{ translateX: 8 }] });
   });
 
   it('translate-y-2', () => {
-    tx('translate-y-2');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('translate-y-2');
     // inspectTestElement('translate-y-2', tw.target, result);
 
     expect(result.base).toStrictEqual({ transform: [{ translateX: 0 }, { translateY: 32 }] });
   });
 
   it('-translate-y-2', () => {
-    tx('-translate-y-2');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('-translate-y-2');
     // inspectTestElement('translate-y-2', tw.target, result);
 
     expect(result.base).toStrictEqual({ transform: [{ translateX: 0 }, { translateY: -32 }] });
   });
 
   it('justify-center', () => {
-    tx('justify-center');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('justify-center');
     // inspectTestElement('justify-center', tw.target, result);
 
     expect(result.base).toStrictEqual({ justifyContent: 'center' });
   });
 
   it('hover:bg-gray-200', () => {
-    tx('hover:bg-gray-200');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('hover:bg-gray-200');
     // inspectTestElement('hover:bg-gray-200', tw.target, result);
     expect(result.pointer).toStrictEqual({ backgroundColor: 'rgba(229,231,235,1)' });
   });
 
   it('first:bg-gray-200', () => {
-    tx('first:bg-gray-200');
-    const result = CssResolver(tw.target, context);
+    const result = generateStylesFor('first:bg-gray-200');
     // inspectTestElement('first:bg-gray-200', tw.target, result);
 
     expect(result.first).toStrictEqual({ backgroundColor: 'rgba(229,231,235,1)' });
--- packages/css/test/test-utils.ts
+++ packages/css/test/test-utils.ts
@@ -1,5 +1,8 @@
 /* eslint-disable no-console */
+import { initialize } from '@universal-labs/twind-adapter';
 import util from 'util';
+import { CssResolver } from '../src';
+import { createParserContext } from '../src/parsers/Parser';
 
 export const inspectTestElement = (msg: string, target: string[], result: any) => {
   console.log(
@@ -14,4 +17,18 @@ export const inspectTestElement = (msg: string, target: string[], result: any) =
       true,
     ),
   );
+};
+
+export const generateStylesFor = (classNames: string) => {
+  const { tx, tw } = initialize();
+  const { context } = createParserContext({
+    deviceHeight: 1280,
+    deviceWidth: 720,
+    rem: 16,
+    platform: 'ios',
+  });
+  tx(classNames);
+  const target = [...tw.target];
+  tw.clear();
+  return CssResolver(target, context);
 };
