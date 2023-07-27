c39e42dd172d535b7b93b2f7c0a3ca97f574f666
Cristhian Gutierrez <chrisarts.net@gmail.com>
2023-07-26 22:45:17 -0500

    fix(docs): docs site starter

--- site/styles/tw.ts
+++ site/styles/tw.ts
@@ -4,5 +4,11 @@ const tailwind = initialize({
   colors: {
     primary: 'blue',
   },
+  fontFamily: {
+    roboto: ['var(--font-roboto)'],
+    'roboto-bold': ['var(--font-roboto)'],
+    'roboto-medium': ['var(--font-roboto)'],
+    sans: ['var(--font-roboto)'],
+  },
 });
 export default tailwind;
--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     colors: {
       primary: 'blue',
     },
+    fontFamily: {
+      roboto: ['var(--font-roboto)'],
+      'roboto-bold': ['var(--font-roboto)'],
+      'roboto-medium': ['var(--font-roboto)'],
+      sans: ['var(--font-roboto)'],
+    },
   },
   content: ['./pages/**/*.{js,jsx,ts,tsx}', '../../packages/**/*.{js,jsx,ts,tsx}'],
 };
