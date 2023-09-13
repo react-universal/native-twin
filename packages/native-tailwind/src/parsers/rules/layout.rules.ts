f2fd3cb4029b02def113edafc49e706a2dd3ed62
Cristhian Gutierrez <chrisarts.net@gmail.com>
2023-09-13 16:54:59 -0500

    feat(tailwind): clean parsers

--- packages/native-tailwind/src/parsers/rules/layout.rules.ts
+++
@@ -1,8 +0,0 @@
-import * as P from '@universal-labs/css/parser';
-
-const aspectRatioSegment = P.choice([
-  P.literal('square'),
-  P.literal('video'),
-  P.literal('ratio'),
-]);
-export const matchAspectRatioRule = P.sequenceOf([P.literal('aspect-'), aspectRatioSegment]);
--- packages/native-tailwind/src/parsers/rules/spacing.ts
+++
@@ -1,9 +0,0 @@
-import * as P from '@universal-labs/css/parser';
-import { matchMaybeNegative, matchTwSegment } from '../common.parsers';
-
-const marginOrPadding = P.choice([P.literal('p-'), P.literal('m-')]);
-export const matchPaddingOrMargin = P.sequenceOf([
-  matchMaybeNegative,
-  marginOrPadding,
-  matchTwSegment,
-]);
