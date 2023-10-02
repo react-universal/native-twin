07c40449521b836671919df4b63ee430b8a85b2b
Cristhian Gutierrez <chrisarts.net@gmail.com>
2023-10-02 18:09:21 -0500

    break(packages): native twin comments

--- packages/vscode-extension/src/internal/config.ts
+++ packages/vscode-extension/src/internal/config.ts
@@ -1,7 +1,7 @@
 export const typeScriptExtensionId = 'vscode.typescript-language-features';
-export const pluginId = '@universal-labs/ts-styled-plugin-tw';
+export const pluginId = '@universal-labs/native-twin-ts-plugin';
 export const configurationSection = 'nativeTailwind';
-const packageName = '@universal-labs/vscode';
+const packageName = '@universal-labs/native-twin-vscode';
 const publisher = 'universal-labs';
 export const extensionChannelName = 'Native Tailwind IntelliSense';
 export const extensionName = `${publisher}.${packageName}`;
