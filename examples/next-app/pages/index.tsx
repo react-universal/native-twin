a6999aaecde00bd9591363ee4fc1f7f8e13dc380
Cristhian Gutierrez <chrisarts.net@gmail.com>
2023-10-11 15:12:09 -0500

    break(styled): unify-rnw

--- examples/next-app/pages/index.tsx
+++ examples/next-app/pages/index.tsx
@@ -13,12 +13,12 @@ const View = styled.View``;
 const IndexPage = () => {
   return (
     <View className='items-center justify-center bg-gray-900 flex-1'>
-      <View className='bg-slate-300 px-8 justify-center ios:items-center py-5 rounded-xl'>
+      <View className='bg-slate-300 px-8 justify-center ios:items-center py-5 rounded-xl group'>
         <H1 className='text(4xl group-hover:(blue-200) sm:5xl) font-bold -translate-1 scale-50 first-letter:mx-10'>
           Card title
         </H1>
         <View>
-          <P className='text(2xl gray-100) hover:font-700'>Subtitle</P>
+          <P className='text(2xl gray-100) hover:font-bold'>Subtitle</P>
           <P className='text(lg) text-red-500 min-w-full'>Card description</P>
         </View>
       </View>
