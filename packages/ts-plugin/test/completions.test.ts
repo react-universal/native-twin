667925532263d4182a2f26b6c818991367984e0e
Cristhian Gutierrez <chrisarts.net@gmail.com>
2023-10-30 21:49:04 -0500

    break(packages): twin package names

--- packages/ts-plugin/test/completions.test.ts
+++ packages/ts-plugin/test/completions.test.ts
@@ -14,12 +14,11 @@ describe('Completions', () => {
   it('Completions for empty string', async () => {
     const server = createServerWithMockFile('const q = css`bg`');
     server.sendCommand('completions', { file: mockFileName, offset: 17, line: 1 });
-
     await server.close();
     const completionsResponse = getFirstResponseOfType('completions', server);
     // console.log('RESPONSE: ', JSON.stringify(completionsResponse, null, 2));
     expect(completionsResponse.success).toBeFalsy();
-  });
+  }, 20000);
 
   // it('Completions for partial className', () => {
   //   const server = createServerWithMockFile('const q = css`bg-`');
