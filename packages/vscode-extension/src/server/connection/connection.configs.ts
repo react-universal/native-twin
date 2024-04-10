21381cde3f8bc91e455a3d3b42328c291044d778
Christian Gutierrez <chrisarts.net@gmail.com>
2024-04-09 21:07:24 -0500

    feat(vscode): Extension config

--- packages/vscode-extension/src/server/connection/connection.configs.ts
+++ packages/vscode-extension/src/server/connection/connection.configs.ts
@@ -1,6 +1,4 @@
-import { Hash } from 'effect';
 import * as Data from 'effect/Data';
-import * as Equal from 'effect/Equal';
 
 interface ServerConfig {
   readonly hasConfigurationCapability: boolean;
@@ -13,34 +11,3 @@ export class ServerConfigState extends Data.TaggedClass('ServerConfigState')<Ser
     return new ServerConfigState({ ...values });
   }
 }
-
-const a = Data.struct({
-  a: 1,
-});
-
-const b = Data.struct({
-  a: 1,
-});
-
-a === b; // ?
-Equal.equals(a, b); // ?
-
-class AA implements Equal.Equal {
-  constructor(readonly name: string) {}
-
-  [Equal.symbol](that: Equal.Equal): boolean {
-    if (that instanceof AA) {
-      return Equal.equals(this.name, that.name);
-    }
-    return false;
-  }
-
-  [Hash.symbol](): number {
-    return this.name.length;
-  }
-}
-
-const aa = new AA('Chris');
-const bb = new AA('Chris');
-
-Equal.equals(aa, bb); //?
