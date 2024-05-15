46153d180e65d24e8b43a059c90b0931f406dc47
Christian Gutierrez <chrisarts.net@gmail.com>
2024-05-14 20:38:55 -0500

    feat(LSP): LSP document color provider

--- packages/language-server/src/language/utils/completion.pipes.ts
+++ packages/language-server/src/language/utils/completion.pipes.ts
@@ -1,10 +1,14 @@
+import { TinyColor } from '@ctrl/tinycolor';
 import * as ReadonlyArray from 'effect/Array';
 import { pipe } from 'effect/Function';
 import * as HashSet from 'effect/HashSet';
 import * as Option from 'effect/Option';
 import * as vscode from 'vscode-languageserver/node';
 import { TemplateNode, TwinDocument } from '../../documents/document.resource';
-import { NativeTwinManagerService } from '../../native-twin/native-twin.models';
+import {
+  NativeTwinManager,
+  NativeTwinManagerService,
+} from '../../native-twin/native-twin.models';
 import { LocatedGroupTokenWithText } from '../../template/template.models';
 import { TwinVariantCompletion } from '../../types/native-twin.types';
 import { TemplateTokenData } from '../language.models';
@@ -119,5 +123,46 @@ export const getTokensAtOffset = (node: TemplateNode, offset: number) => {
       (x) => offset >= x.token.bodyLoc.start && offset <= x.token.bodyLoc.end,
     ),
     ReadonlyArray.dedupe,
+  );
+};
+
+export const getDocumentTemplatesColors = (
+  templates: TemplateNode[],
+  twinService: NativeTwinManager,
+  twinDocument: TwinDocument,
+) => {
+  return pipe(
+    templates,
+    ReadonlyArray.dedupe,
+    ReadonlyArray.flatMap((template) => template.parsedNode),
+    ReadonlyArray.flatMap((x) => {
+      const flatten = getFlattenTemplateToken(x);
+      return flatten;
+    }),
+    ReadonlyArray.dedupe,
+    ReadonlyArray.flatMap((x) => templateTokenToColorInfo(x, twinService, twinDocument)),
+  );
+};
+
+const templateTokenToColorInfo = (
+  templateNode: TemplateTokenData,
+  twinService: NativeTwinManager,
+  twinDocument: TwinDocument,
+): vscode.ColorInformation[] => {
+  return twinService.completions.twinRules.pipe(
+    HashSet.filter((y) => y.completion.className === templateNode.token.text),
+    ReadonlyArray.fromIterable,
+    ReadonlyArray.map((completion) => ({ node: templateNode, completion })),
+    ReadonlyArray.filter((x) => x.completion.rule.themeSection === 'colors'),
+    ReadonlyArray.map((x): vscode.ColorInformation => {
+      const color = new TinyColor(x.completion.completion.declarationValue).toRgb();
+      return {
+        range: vscode.Range.create(
+          twinDocument.handler.positionAt(x.node.token.bodyLoc.start),
+          twinDocument.handler.positionAt(x.node.token.bodyLoc.end),
+        ),
+        color: vscode.Color.create(color.r / 255, color.g / 255, color.b / 255, color.a),
+      };
+    }),
   );
 };
