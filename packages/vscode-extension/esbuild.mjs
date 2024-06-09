52e6155489b0b6e431fe84e0ba2bd6068db77fa9
Christian Gutierrez <chrisarts.net@gmail.com>
2024-06-09 04:30:22 -0500

    break(turbo): using turbo

--- packages/vscode-extension/esbuild.mjs
+++ packages/vscode-extension/esbuild.mjs
@@ -12,6 +12,20 @@ const context = await esbuild.context({
   bundle: true,
   platform: 'node',
   external: ['vscode'],
+
+  format: 'cjs',
+  logLevel: 'info',
+  minify: true,
+});
+
+const serverContext = await esbuild.context({
+  entryPoints: ['src/servers/native-twin.server.ts'],
+  outdir: 'build',
+  sourcemap: 'both',
+  bundle: true,
+  platform: 'node',
+  external: ['vscode'],
+
   format: 'cjs',
   logLevel: 'info',
   minify: true,
@@ -19,6 +33,7 @@ const context = await esbuild.context({
 
 console.log('- Building');
 await context.rebuild();
+await serverContext.rebuild();
 
 if (args.watch) {
   console.log('- Watching');
@@ -26,4 +41,5 @@ if (args.watch) {
 } else {
   console.log('- Cleaning up');
   await context.dispose();
+  await serverContext.dispose();
 }
--- packages/vscode-extension/project-fixture/index.ts
+++ packages/vscode-extension/project-fixture/index.ts
@@ -1,1 +1,2 @@
-css`md:bg-red-100/50 bg(md:black rose-50/50 hover:pink-100)`
+css`md:bg-red-100 bg(md:black rose-50/50 hover:pink-100) text-xs text-[10px]`
+custom`bg`
