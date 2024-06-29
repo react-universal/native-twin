f8e77c134684a639063f4e230394035179d73d8a
Christian Gutierrez <chrisarts.net@gmail.com>
2024-06-29 00:33:56 -0500

    break(turbo): using turbo

--- packages/babel/babel.js
+++ packages/babel/babel.js
@@ -1,14 +1,5 @@
 module.exports = function () {
   return {
-    plugins: [
-      require('./build').default,
-      [
-        '@babel/plugin-transform-react-jsx',
-        {
-          runtime: 'automatic',
-          importSource: '@native-twin/jsx',
-        },
-      ],
-    ],
+    plugins: [require('./build').default],
   };
 };
