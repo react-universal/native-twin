4ca04cd0b102c9cdf499bee26131bfbcdba1d6fd
MarcialFantini <marcialfantini@gmail.com>
2024-09-12 01:50:36 -0300

    feat(navbar): add links and links navbar

--- sites/native-twin-docs/src/components/Navbar/index.tsx
+++ sites/native-twin-docs/src/components/Navbar/index.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+import { IoLogoGithub } from "react-icons/io5";
 
 export const Navbar = () => {
   return (
@@ -11,10 +13,11 @@ export const Navbar = () => {
           <h3>  Native-Twin </h3>
         </div> 
         <nav className="w-full ">
-          <ul className="flex flex-row gap-4 justify-end">
-            <li>Docs</li>
-            <li>Components</li>
-            <li>Github button</li>
+          <ul className="flex flex-row gap-4 items-center justify-end">
+            <li><Link href={"/"}>Home</Link></li>
+            <li ><Link href={'/docs'}>Docs</Link></li>
+            
+            <li><Link  href={"https://github.com/react-universal/native-twin"}><IoLogoGithub  className="w-5 h-5 object-contain"/></Link></li>
             {/* <li>Theme button</li> preguntar chrispio */}
           </ul>
         </nav>
